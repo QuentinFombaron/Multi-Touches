e7435f74531fe712a881f66ce7f2a2f88cddcbf2
leovalette <leovalette38@gmail.com>
2018-10-26 10:43:07 +0200

    Add Rotozoom and drag

--- ts/transfo.ts
+++ ts/transfo.ts
@@ -1,71 +1,90 @@
 let re_matrix = /^matrix\((.*), (.*), (.*), (.*), (.*), (.*)\)$/;
 
 let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-let idM	= svg.createSVGMatrix();
-idM.a=1; idM.b=0; idM.c=0; idM.d=1; idM.e=0; idM.f=0;
+let idM = svg.createSVGMatrix();
+idM.a = 1; idM.b = 0; idM.c = 0; idM.d = 1; idM.e = 0; idM.f = 0;
 
 //______________________________________________________________________________________________________________________
-export let setMatrixCoordToElement =    ( element: HTMLElement
-                                        , a : number
-                                        , b : number
-                                        , c : number
-                                        , d : number
-                                        , e : number
-                                        , f : number
-                                        ) => {
-    element.style.transform = "matrix(" + a +"," + b +"," + c +"," + d +"," + e +"," + f +")";
+export let setMatrixCoordToElement = (element: HTMLElement
+  , a: number
+  , b: number
+  , c: number
+  , d: number
+  , e: number
+  , f: number
+) => {
+  element.style.transform = "matrix(" + a + "," + b + "," + c + "," + d + "," + e + "," + f + ")";
 };
 
 //______________________________________________________________________________________________________________________
 export let setMatrixToElement = (element: HTMLElement, M: SVGMatrix) => {
-    setMatrixCoordToElement(element, M.a, M.b, M.c, M.d, M.e, M.f);
+  setMatrixCoordToElement(element, M.a, M.b, M.c, M.d, M.e, M.f);
 };
 
 //______________________________________________________________________________________________________________________
-export let getMatrixFromString = (str: string) : SVGMatrix => {
-    let res		= re_matrix.exec( str )
-      , matrix	= svg.createSVGMatrix()
-      ;
-    matrix.a = parseFloat(res[1]) || 1;
-    matrix.b = parseFloat(res[2]) || 0;
-    matrix.c = parseFloat(res[3]) || 0;
-    matrix.d = parseFloat(res[4]) || 1;
-    matrix.e = parseFloat(res[5]) || 0;
-    matrix.f = parseFloat(res[6]) || 0;
+export let getMatrixFromString = (str: string): SVGMatrix => {
+  let res = re_matrix.exec(str)
+    , matrix = svg.createSVGMatrix()
+    ;
+  matrix.a = parseFloat(res[1]) || 1;
+  matrix.b = parseFloat(res[2]) || 0;
+  matrix.c = parseFloat(res[3]) || 0;
+  matrix.d = parseFloat(res[4]) || 1;
+  matrix.e = parseFloat(res[5]) || 0;
+  matrix.f = parseFloat(res[6]) || 0;
 
-    return matrix;
+  return matrix;
 };
 
 //______________________________________________________________________________________________________________________
-export let getPoint = (x: number, y: number) : SVGPoint => {
-    let point = svg.createSVGPoint();
-    point.x = x || 0;
-    point.y = y || 0;
-    return point;
+export let getPoint = (x: number, y: number): SVGPoint => {
+  let point = svg.createSVGPoint();
+  point.x = x || 0;
+  point.y = y || 0;
+  return point;
 };
 
 //______________________________________________________________________________________________________________________
-export let getMatrixFromElement = (element: Element) : SVGMatrix => {
-	return getMatrixFromString( window.getComputedStyle(element).transform || "matrix(1,1,1,1,1,1)" );
+export let getMatrixFromElement = (element: Element): SVGMatrix => {
+  return getMatrixFromString(window.getComputedStyle(element).transform || "matrix(1,1,1,1,1,1)");
 };
 
 //______________________________________________________________________________________________________________________
-export let drag =       ( element               : HTMLElement
-                        , originalMatrix        : SVGMatrix
-                        , Pt_coord_element      : SVGPoint
-                        , Pt_coord_parent       : SVGPoint
-                        ) => {
-	// TO BE DONE
+export let drag = (element: HTMLElement
+  , originalMatrix: SVGMatrix
+  , Pt_coord_element: SVGPoint
+  , Pt_coord_parent: SVGPoint
+) => {
+  var resMatrix : SVGMatrix
+  resMatrix.a = originalMatrix.a;
+  resMatrix.b = originalMatrix.b;
+  resMatrix.c = originalMatrix.c;
+  resMatrix.d = originalMatrix.d;
+  resMatrix.e = Pt_coord_parent.x - originalMatrix.a * Pt_coord_element.x - originalMatrix.c * Pt_coord_element.y;
+  resMatrix.f = Pt_coord_parent.y - originalMatrix.b * Pt_coord_element.x - originalMatrix.d * Pt_coord_element.y;
+  setMatrixToElement(element,resMatrix);
 };
 
 //______________________________________________________________________________________________________________________
-export let rotozoom =   ( element           : HTMLElement
-                        , originalMatrix    : SVGMatrix
-                        , Pt1_coord_element : SVGPoint
-                        , Pt1_coord_parent  : SVGPoint
-                        , Pt2_coord_element : SVGPoint
-                        , Pt2_coord_parent  : SVGPoint
-                        ) => {
-	// TO BE DONE
+export let rotozoom = (element: HTMLElement
+  , originalMatrix: SVGMatrix // /!\ POURQUOI ON NE S4EN SERT PAS ?  
+  , Pt1_coord_element: SVGPoint
+  , Pt1_coord_parent: SVGPoint
+  , Pt2_coord_element: SVGPoint
+  , Pt2_coord_parent: SVGPoint
+) => {
+  var resMatrix : SVGMatrix
+  var dx_element = Pt2_coord_element.x - Pt1_coord_element.x;
+  var dy_element = Pt2_coord_element.y - Pt1_coord_element.y;
+  var dx_parent = Pt2_coord_parent.x - Pt1_coord_parent.x;
+  var dy_parent = Pt2_coord_parent.y - Pt1_coord_parent.y;
+  var s = (dy_parent / dy_element - dx_parent / dx_element) / (dy_element / dy_element - dx_element / dx_element);
+  var c = (dx_parent + s * dy_element) / dx_element;
+  resMatrix.a = c;
+  resMatrix.b = s;
+  resMatrix.c = -s;
+  resMatrix.d = c;
+  resMatrix.e = Pt1_coord_parent.x - c * Pt1_coord_element.x + s * Pt1_coord_element.y;
+  resMatrix.f = Pt1_coord_parent.y - s * Pt1_coord_element.x - c * Pt1_coord_element.y;
+  setMatrixToElement(element,resMatrix);
 };
-
